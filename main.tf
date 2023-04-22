d95c6958bb73d1b60884fe38b2cf64ce9699f163
Mateus Lira <mateus.lira@kxp.consulting>
2023-04-22 11:06:12 -0300

    webapp

--- main.tf
+++ main.tf
@@ -24,3 +24,20 @@ resource "azurerm_storage_account" "myStorage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_service_plan" "myServicePlan" {
+  name                = "myServicePlan"
+  resource_group_name = "sandbox"
+  location            = "eastus"
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "myWebApp" {
+  name                = "liratech-webapp"
+  resource_group_name = "sandbox"
+  location            = "eastus"
+  service_plan_id     = azurerm_service_plan.myServicePlan.id
+
+  site_config {}
+}
